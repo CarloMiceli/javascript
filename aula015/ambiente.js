60ccbe7ed5b22e8dde39fb6df8720aa63a776722
CarloMiceli <134542474+CarloMiceli@users.noreply.github.com>
2023-05-31 11:55:44 -0300

    ex015

---
+++ aula015/ambiente.js
@@ -0,0 +1,8 @@
+let num = [5,8,2,9,3]
+console.log(`Nosso vetor é o ${num[0]}`)
+let pos = num.indexOf(8)
+if (pos == -1) {
+    console.log('O valor não foi encontrado')
+} else {
+    console.log(`O valor 4 está na posição ${pos}`)
+}
